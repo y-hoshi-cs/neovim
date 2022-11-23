51e7e1763d4312ad237b242bfa1d14cd06868b12
y.hoshi <francekakumei714@gmail.com>
2022-11-23 10:21:32 +0000

    basic solarized settings

---
+++ lua/plugins.lua
@@ -0,0 +1,15 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("Packer is not installed")
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'hoob3rt/lualine.nvim' -- StatusLine
+end)
